cb9c04bfa3d4f239c85f0827ee1d5b0cba6cf44d
smnsohail <1smnsohail@gmail.com>
2024-12-13 00:05:23 +0530

    stop watch demo 1st

---
+++ script.js
@@ -0,0 +1,40 @@
+let interval;
+let elapsedTime = 0;
+let isRunning = false;
+
+function start(){
+    if(isRunning) return;
+    isRunning= true;
+    
+    const startTime= Date.now() - elapsedTime;
+
+    interval = setInterval(()=>{
+        elapsedTime = Date.now() - startTime;
+        updateDisplay(elapsedTime);
+    }, 1000);
+}
+
+function stop(){
+    clearInterval(interval); 
+    isRunning = false;
+}
+
+function reset(){
+    clearInterval(interval);
+    isRunning = false;
+    elapsedTime = 0;
+    updateDisplay(elapsedTime);
+}
+
+function updateDisplay(time){
+    const seconds = Math.floor((time / 1000) % 60);
+    const minutes = Math.floor((time / (1000 * 60)) % 60);
+    const hours = Math.floor(time / (1000 * 60 * 60));
+
+    document.getElementById('second').innerText = String(seconds).padStart(2,'0');
+document.getElementById('minute').innerText = String(minutes).padStart(2, '0');
+document.getElementById('hour').innerText = String(hours).padStart(2, '0');
+}
+
+
+
